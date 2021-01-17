a87e2b869640e0b7535f713f4f842c9b292c1a56
KKangTaeHo <58099626+KKangTaeHo@users.noreply.github.com>
2021-01-17 16:19:30 +0900

    딕셔너리, 해시맵

---
+++ CSharpBaseInUnity/Assets/Scripts/DictionaryEx.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DictionaryEx : MonoBehaviour
+{
+    public class User
+    {
+        public string name;
+        public int age;
+    }
+
+    Dictionary<string, User> userDic;
+
+    void Start()
+    {
+        // 1. 인스턴스 생성 안하고 호출시 'NullReferenceException' 호출
+        Debug.Log(userDic["kim"]);
+
+        // 2. 딕셔너리 인스턴스 생성하고  데이터 선언 없이 호출 시 'KeyNotFoundException' 호출
+        userDic = new Dictionary<string, User>();
+        Debug.Log(userDic["kim"]);
+
+        // 3. 선언 후, 데이터가 null 인 경우 메서드 호출시 'NullRefeenceException' 호출
+        userDic.Add("kim", new User());
+        Debug.Log(userDic["kim"].name.Split('-'));
+    }
+
+
+    void Ex01()
+    {
+        // Dictionary(HashMap)와 Hashtable의 차이점
+        // 1. 해시테이블
+        // - Non-Generic
+        // - Key와 Value가 모두 Object를 입력받는다.
+        // - 박싱/언박싱 을 사용함.
+        // 2. 딕셔너리
+        // - Generic
+        // - Key와 Value가 모두 타입을 받는다.
+        // - 박싱/ 언박싱이 안일어난다.
+    }
+}
---
+++ CSharpBaseInUnity/Assets/Scripts/HashSetEx.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HashSetEx : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        HashSet<int> hSet = new HashSet<int>();
+
+        hSet.Add(5);
+        hSet.Add(1);
+        hSet.Add(4);
+        hSet.Add(7);
+        hSet.Add(4);
+        hSet.Add(1);
+
+        // - HashSet은 중복을 허용하지 않는다.
+        // - 그리고 해당요소가 특정 순서로 정렬 되어있지 않다.
+        string s = null;
+        foreach(var v in hSet)
+            s += $"{v}, ";
+
+        Debug.Log(s);   // 5, 1, 4, 7
+    }
+}
